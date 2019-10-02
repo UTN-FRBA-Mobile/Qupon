57dd381ba1d5a2fbc2e5d0ca7b99db889292d5f5
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2019-10-01 21:07:59 -0300

    Agrego navigation graph con todas las pantallas

---
+++ app/src/main/java/edu/utn/mobile/qupon/MainActivity.kt
@@ -0,0 +1,16 @@
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import edu.utn.mobile.qupon.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //setSupportActionBar(toolbar)
+        val host = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction().replace(R.id.mapMainFragment,host).setPrimaryNavigationFragment(host).commit()
+    }
+}

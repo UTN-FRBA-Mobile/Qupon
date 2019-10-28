2b1cad6bda10b578d05da1aeed6e9501877ba1e3
Jonathan Buzzetti <buzzetti.jp@gmail.com>
2019-10-28 13:44:46 -0300

    nuevo fragment con información detallada de cupón

--- app/src/main/java/edu/utn/mobile/qupon/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/edu/utn/mobile/qupon/ui/slideshow/SlideshowFragment.java
@@ -23,6 +23,7 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        /*
         final TextView textView = root.findViewById(R.id.text_slideshow);
         slideshowViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +31,7 @@ public class SlideshowFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        */
         return root;
     }
 }
